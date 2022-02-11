270b2109c67bb2c82051edcc95dcd55676d8cedd
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-02-11 18:08:28 +0100

    add deploy on GHPage

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,16 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+const base = process.env.NODE_ENV === 'production' ? './app_senbilisationWeb/' : './';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url))
+        }
+    },
+    base: base,
 })
