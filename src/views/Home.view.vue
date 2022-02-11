5195982beacfd38329de87c3ca56c72e0cb09d4b
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-02-11 17:42:09 +0100

    first commit

---
+++ src/views/Home.view.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Utilisez vous Internet de manière sécurisé ?</h1>
+    <button>Lancer le test</button>
+  </main>
+</template>
