72bb316da0643e1e30ab131def87022cf3041b86
BCollignonEcv <baptiste.collignon@mail-ecv.fr>
2022-02-17 02:42:59 +0100

    deploy

---
+++ assets/NotFound.view.3e3b9d70.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.798b2401.js";import{o,c as t,a as r}from"./vendor.b2dad31b.js";var s="/app_senbilisationWeb/img/fakeError.png";const n={name:"404",components:{},mounted(){}},a=r("img",{src:s,alt:"",srcset:""},null,-1),c=[a];function _(i,p,m,l,d,f){return o(),t("figure",null,c)}var $=e(n,[["render",_]]);export{$ as default};
